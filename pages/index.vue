99ad87b70b84cf37cf25ad1d04505d12bf82d303
Jason Rivera <jp_rivera_@hotmail.com>
2021-07-07 14:46:46 -0700

    added more pages and routing

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="container">
-    <Hero />
-    <LargeCardDisplay 
-      v-for="cardInfo in largeCardInfo"
-      :key="cardInfo.id"
-      :cardsSection="cardInfo"
-    />
+  <div>
+    <Nav />
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import { largeCardSections } from "@/assets/data.js";
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
 
 export default {
   data() {
     return {
-      largeCardInfo: largeCardSections
-    }
+      largeCardInfo: largeCardSections,
+      smallCardSections
+    };
   }
-}
+};
 </script>
